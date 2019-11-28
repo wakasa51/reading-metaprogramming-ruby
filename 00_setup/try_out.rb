a30cd68e65321e61cf7e64f6dbfaf1a8a481e3cc
wakasa51 <y.takashi51@gmail.com>
2019-11-28 14:46:04 +0900

    testを通す

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -5,4 +5,35 @@ class TryOut
   # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
   # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
   # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+  def initialize(*names)
+    @first_name  = names.first
+    @last_name   = names.last
+    @middle_name = names[1] if names.size > 2
+  end
+
+  def full_name
+    names.join(' ')
+  end
+
+  def first_name=(name)
+    @first_name = name
+  end
+
+  def upcase_full_name
+    names.map(&:upcase).join(' ')
+  end
+
+  def upcase_full_name!
+    @first_name.upcase!
+    @last_name.upcase!
+    @middle_name.upcase! if @middle_name
+
+    upcase_full_name
+  end
+
+  private
+
+  def names
+    [@first_name, @middle_name, @last_name].compact
+  end
 end
